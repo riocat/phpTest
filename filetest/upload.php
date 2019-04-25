499f92134391b12d7206bff96423336ad8573498
rio <hubanya@sina.com>
2019-04-25 15:56:11 +0800

    Modify change test way

--- filetest/upload.php
+++ filetest/upload.php
@@ -1,23 +1,23 @@
 <?php
-/*if (!move_uploaded_file($_FILES['upload_file']['tmp_name'], "../fileTemp/" . $_FILES['upload_file']['name'])) {
+if (!move_uploaded_file($_FILES['upload_file']['tmp_name'], "../fileTemp/" . $_FILES['upload_file']['name'])) {
     echo "error";
 } else {
     echo "success";
-}*/
+}
 
 
 require_once "excelUtil.php";
 
 $execl = new excelUtil();
 
-if( $_FILES['upload_file']['name']) {
-
-}else{
-
+if (empty($_FILES['upload_file']['name'])) {
+    $fs = $_FILES['upload_file']['name'];
+} else {
+    $fs = "../fileTemp/" . "100.xlsx";
 }
 
-$fs = "../fileTemp/" ."100.xlsx";
+//
 
 echo $fs;
 
-$execl->data_import($fs, substr($fs, strrpos($fs, '.')+1));
+$execl->data_import($fs, substr($fs, strrpos($fs, '.') + 1));
